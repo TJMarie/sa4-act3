ad439e4e7cc6bdcea3da8b8673d1c366db31b337
Adrien Jones <tjones69@memphis.edu>
2024-02-06 16:03:22 -0600

    Added loop for guessing multiple numbers

--- guess_number.py
+++ guess_number.py
@@ -3,7 +3,12 @@ number = 10
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+while guess != 'q':
+    if int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        guess = input("Try again: ")
+
+if guess == 'q':
+    print(f"The number was {number}.")
